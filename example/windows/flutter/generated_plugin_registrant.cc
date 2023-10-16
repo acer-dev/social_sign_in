847ea11d7cefe76167abd74fb45e1b127b7967aa
Caty Chang <caty.chang@acer.com>
2023-10-16 15:41:43 +0800

    modify platform detection

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <desktop_webview_window/desktop_webview_window_plugin.h>
-#include <social_sign_in/social_sign_in_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DesktopWebviewWindowPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DesktopWebviewWindowPlugin"));
-  SocialSignInPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("SocialSignInPluginCApi"));
 }
