a8597886576f67f2ec23f173f2d6d7ea76cf5a2a
Caty Chang <caty.chang@acer.com>
2023-10-16 15:12:49 +0800

    add Google sign in interface

---
+++ lib/site/google/google_sign_in_config.dart
@@ -0,0 +1,26 @@
+import '../../social_sign_in.dart';
+
+class GoogleSignInConfig extends SocialSignInSiteConfig{
+  @override
+  SocialPlatform get site => SocialPlatform.google;
+
+  @override
+  String clientId;
+
+  @override
+  String clientSecret;
+
+  @override
+  String redirectUrl;
+
+  @override
+  List<String> scope;
+
+  GoogleSignInConfig({
+    required this.clientId,
+    required this.clientSecret,
+    required this.redirectUrl,
+    this.scope = const["profile","email"]
+  });
+
+}
