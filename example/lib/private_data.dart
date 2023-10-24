bb0eff56efaad641c714bb12aff7d303da46ab8c
Caty Chang <caty.chang@acer.com>
2023-10-24 14:21:05 +0800

    add SocialPrivateData class

---
+++ example/lib/private_data.dart
@@ -0,0 +1,20 @@
+library private_data;
+
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+class SocialPrivateData {
+  static String simpleRedirectUrl = '';
+  static String appleRedirectUrl = '';
+  static String appleHostUrl = '';
+  static String appleServiceId = '';
+  static String microsoftClientId = '';
+  static String microsoftClientSecret = '';
+  static String facebookClientId = '';
+  static String facebookClientSecret = '';
+  static String googleClientId = '';
+  static String googleClientSecret = '';
+
+}
