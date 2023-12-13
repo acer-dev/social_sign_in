52520b9b14cd3824a0fb335a32f47e2d9fc3e6cc
Caty Chang <caty.chang@acer.com>
2023-12-13 11:55:07 +0800

    updated API documentation

--- lib/src/site/apple/apple_sign_in.dart
+++ lib/src/site/apple/apple_sign_in.dart
@@ -40,7 +40,7 @@ class AppleSignIn extends SocialSignInSite {
     return "";
   }
 
-  ///Calculate hash from string or binary.
+  /// Returns the sha256 hash of [input] in hex notation.
   String sha256ofString(String input) {
     final bytes = utf8.encode(input);
     final digest = sha256.convert(bytes);
